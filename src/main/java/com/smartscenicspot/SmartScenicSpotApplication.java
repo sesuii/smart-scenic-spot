2292cc05313b39bed84a317573c6db3b045d321f
songjiah <2316020516@qq.com>
2023-02-25 16:24:42 +0800

    first commit

---
+++ src/main/java/com/smartscenicspot/SmartScenicSpotApplication.java
@@ -0,0 +1,13 @@
+package com.smartscenicspot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartScenicSpotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartScenicSpotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/smartscenicspot/SmartScenicSpotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartscenicspot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartScenicSpotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
